1e966540eac1f14d8ed54a2cf542f729f3b32bd2
KrunalLathiya <krunallathiya21@gmail.com>
2017-07-11 18:48:34 +0530

    Basic validation complete

---
+++ resources/views/itemForm.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Item Validation Form</title>
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
+  <body>
+    <div class="container">
+      <br />
+      @if ($errors->any())
+          <div class="alert alert-danger">
+              <ul>
+                  @foreach ($errors->all() as $error)
+                      <li>{{ $error }}</li>
+                  @endforeach
+              </ul>
+          </div>
+        @endif
+      <form method="post" action="{{url('items/add')}}">
+        {{csrf_field()}}
+        <div class="form-group">
+          <label for="itemName">Item Name:</label>
+          <input type="text" class="form-control" id="itemName" name="itemName">
+        </div>
+        <div class="form-group">
+          <label for="price">Price:</label>
+          <input type="text" class="form-control" id="price" name="price">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+    </div>
+  </body>
+</html>
